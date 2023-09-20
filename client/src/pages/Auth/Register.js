324220c5e9b4f05d1f3646f1e94916805963e6bb
MianAafi <aftabfarooq8@gmail.com>
2023-09-21 00:42:45 +0500

    add register

--- client/src/pages/Auth/Register.js
+++ client/src/pages/Auth/Register.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import Layout from './../../components/Layout/Layout';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
-import '../../styles/AuthStyles.css';
+import '../../styles/Register.css';
+import Header from '../../components/Layout/Header';
+
 const Register = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
@@ -38,83 +40,92 @@ const Register = () => {
   };
 
   return (
-    <Layout title="Register - Ecommer App">
-      <div className="form-container register" style={{ minHeight: '90vh' }}>
+    <>
+      <Header />
+      <div className="Registeration">
         <form onSubmit={handleSubmit}>
-          <h4 className="title">REGISTER FORM</h4>
-          <div className="mb-3">
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="Enter Your Name"
-              required
-              autoFocus
-            />
+          <div className="register-logo">
+            <h6>WELCOME TO</h6>
+            <img src="./images/Logo.png" alt="logo" />
           </div>
-          <div className="mb-3">
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="Enter Your Email "
-              required
-            />
+          <div className="register-form">
+            <h6 className="title">REGISTER FORM</h6>
+            <div className="mb-3">
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="form-control"
+                id="exampleInputEmail1"
+                placeholder="Enter Your Name"
+                required
+                autoFocus
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="form-control"
+                id="exampleInputEmail1"
+                placeholder="Enter Your Email "
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="form-control"
+                id="exampleInputPassword1"
+                placeholder="Enter Your Password"
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="text"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                className="form-control"
+                id="exampleInputEmail1"
+                placeholder="Enter Your Phone"
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="text"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                className="form-control"
+                id="exampleInputEmail1"
+                placeholder="Enter Your Address"
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="text"
+                value={answer}
+                onChange={(e) => setAnswer(e.target.value)}
+                className="form-control"
+                id="exampleInputEmail1"
+                placeholder="What is Your Favorite sports"
+                required
+              />
+            </div>
+            <div className="register-btn">
+              <button type="submit">REGISTER</button>
+            </div>
+            <p className="alreadyaccount">Already have an Account?</p>
+            <Link to="/login">Login</Link>
           </div>
-          <div className="mb-3">
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="form-control"
-              id="exampleInputPassword1"
-              placeholder="Enter Your Password"
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="text"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="Enter Your Phone"
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="text"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="Enter Your Address"
-              required
-            />
-          </div>
-          <div className="mb-3">
-            <input
-              type="text"
-              value={answer}
-              onChange={(e) => setAnswer(e.target.value)}
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="What is Your Favorite sports"
-              required
-            />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            REGISTER
-          </button>
         </form>
       </div>
-    </Layout>
+    </>
   );
 };
 
